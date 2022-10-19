a0ea5df7fe5ec989db2b30ba5f4588c1ab650306
Lukassza <lukasszafranek@gmail.com>
2022-10-19 14:29:45 +0200

    todolist v2

--- todolist.js
+++ todolist.js
@@ -1,40 +1,46 @@
 let elemlist = document.querySelector("#elemlist");
 let result = document.querySelector(".result");
-
 let ajouter = document.querySelector("#ajouter");
 
+
+//! Ajout //
 ajouter.addEventListener("click", function (e) {
   e.preventDefault();
     let liste = document.createElement('p')
     liste.textContent =`${elemlist.value}`
-    liste.id="list"+document.querySelectorAll('.result list').length
-    liste.style='color:red;'
+    liste.id="liste"+document.querySelectorAll('.result p').length
+    liste.style='color:white;'
     result.appendChild(liste)
-//   result.textcontent = `${elemlist.value}`;
-});
-// result.innerHTML = `${elemlist.value}`;
+
+    // Bouton supprimer //
+
+    let supprimer = document.createElement('Button')
+    supprimer.innerHTML="X"
+    supprimer.id="supprimer"+document.querySelectorAll('.result p').length
+    supprimer.classList.add("SUP")
+    liste.appendChild(supprimer)
+
+    // Bouton modifier//
+    
+    let modifier = document.createElement('Button')
+    modifier.innerHTML="Modifier"
+    modifier.id="modifier"+document.querySelectorAll('.result p').length
+    modifier.classList.add("MOD")
+    liste.appendChild(modifier)
+})
 
 
 
 
+//!MODIFICATION //
+// modifier.addEventListener("click",function(){
+//   modifier.parentElement.
+// })
 
-
-
-
-
-// addLorem.addEventListener('click', function(){
-//     //je défini mon nouvel élément
-//     let newP = document.createElement('p') // création en mémoire
-//     newP.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ipsam vitae nulla facilis possimus molestiae necessitatibus optio ab nesciunt voluptatibus, ratione quasi laborum. Corrupti, beatae odit! Possimus a odio voluptatem."
-//     newP.id = "newP"+document.querySelectorAll('#result p').length
-//     newP.style='color:red;'
-//     // une fois prêt, je l'ajoute a mon doument HTML
-//     result.appendChild(newP)
-
-
-
-
-
+//! Supression //
+supprimer.addEventListener("click", function() {
+  supprimer.parentElement.remove()
+})
 
 
 
