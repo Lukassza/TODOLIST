39533edfbab1d039b71c35ba183a493c97c5e01e
Lukassza <lukasszafranek@gmail.com>
2022-10-19 16:11:46 +0200

    TODOLIST V3

--- todolist.js
+++ todolist.js
@@ -1,7 +1,7 @@
 let elemlist = document.querySelector("#elemlist");
 let result = document.querySelector(".result");
 let ajouter = document.querySelector("#ajouter");
-
+let barre = false
 
 //! Ajout //
 ajouter.addEventListener("click", function (e) {
@@ -9,6 +9,7 @@ ajouter.addEventListener("click", function (e) {
     let liste = document.createElement('p')
     liste.textContent =`${elemlist.value}`
     liste.id="liste"+document.querySelectorAll('.result p').length
+    liste.classList.add("LIS")
     liste.style='color:white;'
     result.appendChild(liste)
 
@@ -19,28 +20,38 @@ ajouter.addEventListener("click", function (e) {
     supprimer.id="supprimer"+document.querySelectorAll('.result p').length
     supprimer.classList.add("SUP")
     liste.appendChild(supprimer)
-
+    supprimer.addEventListener("click", function() {
+    supprimer.parentElement.remove();
+})
     // Bouton modifier//
-    
+
     let modifier = document.createElement('Button')
     modifier.innerHTML="Modifier"
     modifier.id="modifier"+document.querySelectorAll('.result p').length
     modifier.classList.add("MOD")
     liste.appendChild(modifier)
-})
+    
+    // modifier.addEventListener("click",function(){
+    // modifier.parentElement.
+    // })
 
-
-
-
-//!MODIFICATION //
-// modifier.addEventListener("click",function(){
-//   modifier.parentElement.
-// })
-
-//! Supression //
-supprimer.addEventListener("click", function() {
-  supprimer.parentElement.remove()
-})
+    let cocher = document.createElement('input')
+      cocher.setAttribute("type","checkbox")
+      cocher.classList.add("COCHER")
+      liste.appendChild(cocher)
+      cocher.addEventListener("click",function(){
+        if(barre===false){
+          cocher.parentElement.style="text-decoration:line-through"
+          barre=true
+        }
+        else{
+          cocher.parentElement.style="text-decoration:none"
+          barre=false
+        }
+          
+      })
+    
+    })
 
 
 
